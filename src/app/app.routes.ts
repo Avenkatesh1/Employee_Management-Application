7ad33c6bfaa0e4932611b504ec3efc9aeae4fe46
A Venkatesh <avenkateshanand1998@gmail.com>
2024-09-26 10:37:12 +0530

    In this commit I have Create a needed components and Routing for this components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { LayoutComponent } from './Pages/layout/layout.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './Pages/employee/employee.component';
+import { ProjectComponent } from './Pages/project/project.component';
+import { EmpolyeeProjectComponent } from './Pages/empolyee-project/empolyee-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        redirectTo:"Login",
+        pathMatch:"full"
+    },
+    {
+        path:"Login",
+        component: LoginComponent
+    },
+    {
+        path:"",
+        component: LayoutComponent,
+        children:[
+            {
+                path:"Dashboard",
+                component: DashboardComponent
+            },
+            {
+                path:"Employee",
+                component: EmployeeComponent
+            },
+            {
+                path:"Project",
+                component: ProjectComponent
+            },
+            {
+                path:"Employee-Project",
+                component: EmpolyeeProjectComponent
+            }
+        ]
+    }
+];
