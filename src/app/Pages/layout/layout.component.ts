6f58dd784ef0813a73acc8094e7bd89611c62f38
A Venkatesh <avenkateshanand1998@gmail.com>
2024-09-26 12:12:09 +0530

    Created a Navbar and show it to the Layout Component

--- src/app/Pages/layout/layout.component.ts
+++ src/app/Pages/layout/layout.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  
 
+  router = inject(Router);
+
+  logOff(){
+    this.router.navigate(['/Login']);
+  }
 }
