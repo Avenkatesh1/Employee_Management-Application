22ed177625242f254c065741036aefa20922b493
A Venkatesh <avenkateshanand1998@gmail.com>
2024-09-28 19:15:19 +0530

    I Have Created Dashboard In This Dashboard I have To display the total employees and projects

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, Signal } from '@angular/core';
+import { IDashboard, IProject } from '../../model/interface/master';
+import { EmployeeService } from '../../Services/employee.service';
+import { ProjectService } from '../../Services/project.service';
+import { Employees } from '../../model/class/employee';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +11,38 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  dashboardList!: IDashboard;
+  empService = inject(EmployeeService);
+  ProjService = inject(ProjectService);
+  ProjectList = signal<IProject[]>([]);
+  empList = signal<Employees[]>([]);
+
+  ngOnInit(): void {
+    this.getDashboardData();
+    this.getAllProjects();
+    this.getAllEmployees();
+  }
+
+  getDashboardData(){
+    this.empService.getDashboard().subscribe((data:IDashboard) => {
+      this.dashboardList = data;
+  });
+  }
+
+  
+  getAllEmployees(){
+    this.empService.getAllEmployee().subscribe((res:Employees[])=>{
+      this.empList.set(res);
+    });
+        
+  }
+
+  getAllProjects() {
+    this.ProjService.getAllProjects().subscribe((res: IProject[]) => {
+      this.ProjectList.set(res);
+    });
+  }
 
 }
